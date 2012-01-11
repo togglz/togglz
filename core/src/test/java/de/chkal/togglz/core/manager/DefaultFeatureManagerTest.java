24948241a283364a765e19903402d3b5df3753e9
Christian Kaltepoth <christian@kaltepoth.de>
2012-01-11 06:55:31 +0100

    Fixed unit test

--- core/src/test/java/de/chkal/togglz/core/manager/DefaultFeatureManagerTest.java
+++ core/src/test/java/de/chkal/togglz/core/manager/DefaultFeatureManagerTest.java
@@ -9,29 +9,38 @@ import org.junit.Before;
 import org.junit.Test;
 
 import de.chkal.togglz.core.Feature;
+import de.chkal.togglz.core.annotation.EnabledByDefault;
 import de.chkal.togglz.core.config.FeatureManagerConfiguration;
 import de.chkal.togglz.core.repository.FeatureStateRepository;
 import de.chkal.togglz.core.repository.mem.InMemoryRepository;
+import de.chkal.togglz.core.user.FeatureUser;
 import de.chkal.togglz.core.user.FeatureUserProvider;
-import de.chkal.togglz.core.user.NoOpFeatureUserProvider;
+import de.chkal.togglz.core.user.SimpleFeatureUser;
 
 public class DefaultFeatureManagerTest {
 
     private FeatureStateRepository repository;
     private FeatureManager manager;
+    private TestFeatureUserProvider featureUserProvider;
 
     @Before
     public void before() {
+
         repository = new InMemoryRepository();
         repository.setFeatureState(new FeatureState(MyFeatures.DELETE_USERS, true, Arrays.asList("admin")));
         repository.setFeatureState(new FeatureState(MyFeatures.EXPERIMENTAL, false));
-        manager = new DefaultFeatureManager(new MyConfiguration());
+
+        featureUserProvider = new TestFeatureUserProvider();
+
+        manager = new DefaultFeatureManager(new MyConfiguration(featureUserProvider));
+
     }
 
     @After
     public void after() {
         repository = null;
         manager = null;
+        featureUserProvider = null;
     }
 
     @Test
@@ -44,12 +53,20 @@ public class DefaultFeatureManagerTest {
     @Test
     public void testIsActive() {
 
-        // DELETE_USERS only active for admin
+        // DELETE_USERS disabled for unknown user
+        featureUserProvider.setFeatureUser(null);
         assertEquals(false, manager.isActive(MyFeatures.DELETE_USERS));
-        assertEquals(false, manager.isActive(MyFeatures.DELETE_USERS));
+
+        // DELETE_USERS enabled for admin user
+        featureUserProvider.setFeatureUser(new SimpleFeatureUser("admin", false));
         assertEquals(true, manager.isActive(MyFeatures.DELETE_USERS));
 
+        // DELETE_USERS enabled for other user
+        featureUserProvider.setFeatureUser(new SimpleFeatureUser("somebody", false));
+        assertEquals(false, manager.isActive(MyFeatures.DELETE_USERS));
+
         // EXPERIMENTAL disabled for all
+        featureUserProvider.setFeatureUser(null);
         assertEquals(false, manager.isActive(MyFeatures.EXPERIMENTAL));
 
     }
@@ -69,6 +86,12 @@ public class DefaultFeatureManagerTest {
      */
     private final class MyConfiguration implements FeatureManagerConfiguration {
 
+        private final FeatureUserProvider featureUserProvider;
+
+        public MyConfiguration(FeatureUserProvider featureUserProvider) {
+            this.featureUserProvider = featureUserProvider;
+        }
+
         public Class<? extends Feature> getFeatureClass() {
             return MyFeatures.class;
         }
@@ -79,7 +102,25 @@ public class DefaultFeatureManagerTest {
 
         @Override
         public FeatureUserProvider getFeatureUserProvider() {
-            return new NoOpFeatureUserProvider();
+            return featureUserProvider;
+        }
+
+    }
+
+    /**
+     * {@link FeatureUserProvider} that allows to set the user directly
+     */
+    private final class TestFeatureUserProvider implements FeatureUserProvider {
+
+        private FeatureUser featureUser;
+
+        public void setFeatureUser(FeatureUser featureUser) {
+            this.featureUser = featureUser;
+        }
+
+        @Override
+        public FeatureUser getCurrentUser() {
+            return featureUser;
         }
 
     }
@@ -88,8 +129,11 @@ public class DefaultFeatureManagerTest {
      * Feature under test
      */
     private static enum MyFeatures implements Feature {
-        
-        DELETE_USERS, EXPERIMENTAL;
+
+        @EnabledByDefault
+        DELETE_USERS,
+
+        EXPERIMENTAL;
 
     }
 
