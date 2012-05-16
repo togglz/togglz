acc7575bcfaa7925f9a435edf09a0cdda7c59fb1
Christian Kaltepoth <christian@kaltepoth.de>
2012-05-16 17:34:48 +0200

    First version of JNDIFeatureManagerProvider

---
+++ core/src/main/java/org/togglz/core/jndi/JNDIFeatureManagerProvider.java
@@ -0,0 +1,46 @@
+package org.togglz.core.jndi;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.togglz.core.manager.FeatureManager;
+import org.togglz.core.spi.FeatureManagerProvider;
+
+/**
+ * 
+ * Implementation of {@link FeatureManagerProvider} that tries to look up a {@link FeatureManager} from JNDI using the name
+ * <code>java:/comp/env/FeatureManager</code>.
+ * 
+ * @author Christian Kaltepoth
+ * 
+ */
+public class JNDIFeatureManagerProvider implements FeatureManagerProvider {
+
+    public final static String JNDI_NAME = "java:/comp/env/FeatureManager";
+
+    private final Logger log = LoggerFactory.getLogger(JNDIFeatureManagerProvider.class);
+
+    @Override
+    public int priority() {
+        // runs AFTER the WebAppFeatureManagerProvider
+        return 200;
+    }
+
+    @Override
+    public FeatureManager getFeatureManager() {
+
+        try {
+
+            InitialContext initialContext = new InitialContext();
+            return (FeatureManager) initialContext.lookup(JNDI_NAME);
+
+        } catch (NamingException e) {
+            log.debug("FeatureMananger not found: ", e.getMessage());
+        }
+
+        return null;
+
+    }
+}
