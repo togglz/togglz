07d2fb7870dcc52c44b89da189d7e27e3f37a1b4
Christian Kaltepoth <christian@kaltepoth.de>
2012-06-04 07:26:35 +0200

    Minor API change for DeltaSpikeUserProvider

--- deltaspike/src/main/java/org/togglz/deltaspike/DeltaSpikeUserProvider.java
+++ deltaspike/src/main/java/org/togglz/deltaspike/DeltaSpikeUserProvider.java
@@ -9,8 +9,8 @@ import org.togglz.core.user.UserProvider;
 /**
  * 
  * Initial integration for Apache DeltaSpike. Please not that this implementation currently doesn't support the feature admin
- * flag and therefore always sets it to <code>false</code>. Overriding {@link #isFeatureAdmin(String)} allows to change the
- * default behaviour.
+ * flag and therefore always sets it to <code>false</code>. Overriding {@link #isFeatureAdmin(Identity)} allows to change the
+ * default behavior.
  * 
  * @author Christian Kaltepoth
  * 
@@ -27,7 +27,7 @@ public class DeltaSpikeUserProvider implements UserProvider {
 
         if (identity.isLoggedIn()) {
             String name = identity.getUser().getId();
-            return new SimpleFeatureUser(name, isFeatureAdmin(name));
+            return new SimpleFeatureUser(name, isFeatureAdmin(identity));
         }
 
         return null;
@@ -37,10 +37,10 @@ public class DeltaSpikeUserProvider implements UserProvider {
      * Checks if the supplied user is a feature admin. The default implementation always returns <code>false</code>. Users can
      * overwrite this method to implement a different behavior.
      * 
-     * @param user The name of the user
+     * @param identity the identity of the user
      * @return <code>true</code> for feature admins, <code>false</code> otherwise
      */
-    protected boolean isFeatureAdmin(String user) {
+    protected boolean isFeatureAdmin(Identity identity) {
         return false;
     }
 
