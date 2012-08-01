0d7806bc8a264c54c6224c4fb57830ef80bb3f1b
Christian Kaltepoth <christian@kaltepoth.de>
2012-08-01 17:09:06 +0200

    Added new UntypedFeature class for resolving feature enum types lazily

---
+++ core/src/main/java/org/togglz/core/util/UntypedFeature.java
@@ -0,0 +1,50 @@
+package org.togglz.core.util;
+
+import org.togglz.core.Feature;
+import org.togglz.core.context.FeatureContext;
+import org.togglz.core.manager.FeatureManager;
+
+/**
+ * An untyped feature can be used if the name of a feature is known but not the correct type of the feature enum. The real type
+ * of the feature will be resolved lazily by asking the {@link FeatureManager} for the features it is responsible for. If the
+ * name of the untyped feature doesn't correspond to one of these features, a runtime exception will be thrown.
+ * 
+ * @author Christian Kaltepoth
+ */
+public class UntypedFeature implements Feature {
+
+    private final String name;
+
+    public UntypedFeature(String name) {
+        Validate.notBlank(name, "name is required");
+        this.name = name;
+    }
+
+    @Override
+    public String name() {
+        return getTypedFeature().name();
+    }
+
+    @Override
+    public boolean isActive() {
+        return getTypedFeature().isActive();
+    }
+
+    private Feature _feature;
+
+    private Feature getTypedFeature() {
+        if (_feature == null) {
+            Feature[] features = FeatureContext.getFeatureManager().getFeatures();
+            for (Feature f : features) {
+                if (f.name().equals(name)) {
+                    _feature = f;
+                }
+            }
+            if (_feature == null) {
+                throw new IllegalArgumentException("FeatureManager doesn't know about feature: " + name);
+            }
+        }
+        return _feature;
+    }
+
+}
---
+++ core/src/main/java/org/togglz/core/util/Validate.java
@@ -0,0 +1,22 @@
+package org.togglz.core.util;
+
+/**
+ * Common methods for checking method contracts.
+ * 
+ * @author Christian Kaltepoth
+ */
+public class Validate {
+
+    public static void notBlank(String s, String msg) {
+        if (s == null || s.trim().length() == 0) {
+            throw new IllegalArgumentException(msg);
+        }
+    }
+
+    public static void notNull(Object o, String msg) {
+        if (o == null) {
+            throw new IllegalArgumentException(msg);
+        }
+    }
+
+}
