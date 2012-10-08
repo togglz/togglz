73845e87283bb920cd2b3dd352f30fe9239c6604
Christian Kaltepoth <christian@kaltepoth.de>
2012-10-08 17:41:29 +0200

    Integration module for SLF4J

--- core/src/main/java/org/togglz/core/spi/LogProvider.java
+++ core/src/main/java/org/togglz/core/spi/LogProvider.java
@@ -5,7 +5,18 @@ import org.togglz.core.util.Weighted;
 
 /**
  * 
+ * <p>
  * SPI for integrating with other logging frameworks.
+ * </p>
+ * 
+ * <p>
+ * Implementations and their weights:
+ * </p>
+ * 
+ * <ul>
+ * <li>Slf4jLogProvider: 100</li>
+ * <li>Jdk14LogProvider: 1000</li>
+ * </ul>
  * 
  * @author Christian Kaltepoth
  * 
