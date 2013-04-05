b4adde6d5afd6279719382e4cc208dfcd0dcebb4
Christian Kaltepoth <christian@kaltepoth.de>
2013-04-05 07:24:08 +0200

    Added NamedFeature

---
+++ core/src/main/java/org/togglz/core/util/NamedFeature.java
@@ -0,0 +1,30 @@
+package org.togglz.core.util;
+
+import org.togglz.core.Feature;
+import org.togglz.core.context.FeatureContext;
+
+/**
+ * This class can be used if just the name of a feature is known but not the enum type. It is similar to {@link UntypedFeature}
+ * but doesn't try to lazily resolve the type when calling {@link #name()}.
+ * 
+ * @author Christian Kaltepoth
+ */
+public class NamedFeature implements Feature {
+
+    private final String name;
+
+    public NamedFeature(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String name() {
+        return name;
+    }
+
+    @Override
+    public boolean isActive() {
+        return FeatureContext.getFeatureManager().isActive(this);
+    }
+
+}
