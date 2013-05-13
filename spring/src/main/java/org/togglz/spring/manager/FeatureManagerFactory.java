0eb7842585a42c964b24b03bf070c4b617e23f74
Christian Kaltepoth <christian@kaltepoth.de>
2013-05-13 17:42:29 +0200

    First version of a Spring FactoryBean for the FeatureManager

---
+++ spring/src/main/java/org/togglz/spring/manager/FeatureManagerFactory.java
@@ -0,0 +1,80 @@
+package org.togglz.spring.manager;
+
+import org.springframework.beans.factory.FactoryBean;
+import org.togglz.core.Feature;
+import org.togglz.core.manager.FeatureManager;
+import org.togglz.core.manager.FeatureManagerBuilder;
+import org.togglz.core.manager.TogglzConfig;
+import org.togglz.core.repository.StateRepository;
+import org.togglz.core.spi.FeatureProvider;
+import org.togglz.core.user.UserProvider;
+
+/**
+ * 
+ * <p>
+ * {@link FactoryBean} for creating a {@link FeatureManager} managed by Spring.
+ * </p>
+ * 
+ * <p>
+ * Example usage:
+ * </p>
+ * 
+ * <pre>
+ * &lt;bean id="userProvider" class="org.togglz.core.user.NoOpUserProvider" /&gt;
+ * 
+ * &lt;bean id="stateRepository" class="org.togglz.core.repository.mem.InMemoryStateRepository" /&gt;
+ * 
+ * &lt;bean id="featureManager" class="org.togglz.spring.manager.FeatureManagerFactory"&gt;
+ *   &lt;property name="featureEnum" value="org.example.myapp.MyFeatures" /&gt; 
+ *   &lt;property name="stateRepository" ref="stateRepository" /&gt;
+ *   &lt;property name="userProvider" ref="userProvider" /&gt;
+ * &lt;/bean&gt;
+ * </pre>
+ * 
+ * @author Christian Kaltepoth
+ * 
+ */
+public class FeatureManagerFactory implements FactoryBean<FeatureManager> {
+
+    private final FeatureManagerBuilder builder = new FeatureManagerBuilder();
+
+    @Override
+    public FeatureManager getObject() throws Exception {
+        return builder.build();
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return FeatureManager.class;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return false;
+    }
+
+    public void setUserProvider(UserProvider userProvider) {
+        builder.userProvider(userProvider);
+    }
+
+    public void setStateRepository(StateRepository stateRepository) {
+        builder.stateRepository(stateRepository);
+    }
+
+    public void setFeatureEnum(Class<? extends Feature> featureEnum) {
+        builder.featureEnum(featureEnum);
+    }
+
+    public void setName(String name) {
+        builder.name(name);
+    }
+
+    public void setTogglzConfig(TogglzConfig togglzConfig) {
+        builder.togglzConfig(togglzConfig);
+    }
+
+    public void setFeatureProvider(FeatureProvider featureProvider) {
+        builder.featureProvider(featureProvider);
+    }
+
+}
---
+++ spring/src/test/java/org/togglz/spring/test/container/ManagedFeatureManagerTest.java
@@ -0,0 +1,47 @@
+package org.togglz.spring.test.container;
+
+import static org.junit.Assert.assertEquals;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.descriptor.api.Descriptors;
+import org.jboss.shrinkwrap.descriptor.api.spec.servlet.web.WebAppDescriptor;
+import org.jboss.shrinkwrap.resolver.api.DependencyResolvers;
+import org.jboss.shrinkwrap.resolver.api.maven.MavenDependencyResolver;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.togglz.core.context.FeatureContext;
+import org.togglz.spring.test.BasicFeatures;
+import org.togglz.test.Deployments;
+
+@RunWith(Arquillian.class)
+public class ManagedFeatureManagerTest {
+
+    @Deployment
+    public static WebArchive createDeployment() {
+        return Deployments.getBasicWebArchive()
+            .addAsLibrary(Deployments.getTogglzSpringArchive())
+            .addAsLibraries(
+                DependencyResolvers.use(MavenDependencyResolver.class)
+                    .artifact("org.springframework:spring-web:3.0.7.RELEASE")
+                    .resolveAs(JavaArchive.class))
+            .addClass(BasicFeatures.class)
+            .addAsWebInfResource("applicationContext-container.xml")
+            .setWebXML(new StringAsset(
+                Descriptors.create(WebAppDescriptor.class)
+                    .contextParam("org.togglz.LOCAL_FEATURE_MANAGER", "false")
+                    .contextParam("contextConfigLocation", "/WEB-INF/applicationContext*.xml")
+                    .listener("org.springframework.web.context.ContextLoaderListener")
+                    .exportAsString()));
+    }
+
+    @Test
+    public void containerManagedFeatureManagerShouldBeUsedIfAvailable() {
+        assertEquals("I'm managed by Spring",
+            FeatureContext.getFeatureManager().getName());
+    }
+
+}
