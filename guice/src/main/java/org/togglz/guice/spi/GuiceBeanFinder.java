e516cd6c29aa968349c33235577584dd261ba151
Christian Kaltepoth <christian@kaltepoth.de>
2013-09-19 07:46:04 +0200

    Modified GuiceBeanFinder to lookup the Injector from the servlet context attribute

--- guice/src/main/java/org/togglz/guice/spi/GuiceBeanFinder.java
+++ guice/src/main/java/org/togglz/guice/spi/GuiceBeanFinder.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import javax.inject.Inject;
+import javax.servlet.ServletContext;
 
 import org.togglz.core.spi.BeanFinder;
 
@@ -13,22 +13,37 @@ import com.google.inject.Injector;
 import com.google.inject.TypeLiteral;
 
 /**
- *
+ * {@link BeanFinder} implementation for Google Guice. Please note that this implementation only works if you are using
+ * GuiceServletContextListener from Guice Servlet for setting up Guice.
+ * 
  * @author Fábio Franco Uechi
+ * @author Christian Kaltepoth
  */
 public class GuiceBeanFinder implements BeanFinder {
 
-    @Inject
-    Injector injector;
-
     @Override
     public <E> Collection<E> find(Class<E> clazz, Object context) {
-        List<Binding<E>> bindings = injector.findBindingsByType(TypeLiteral.get(clazz));
-        Collection<E> result = new ArrayList<E>(bindings.size());
-        for (int i = 0; i < bindings.size(); i++) {
-            result.add(bindings.get(i).getProvider().get());
+
+        if (context instanceof ServletContext) {
+            ServletContext servletContext = (ServletContext) context;
+
+            Injector injector = (Injector) servletContext.getAttribute(Injector.class.getName());
+            if (injector != null) {
+
+                List<Binding<E>> bindings = injector.findBindingsByType(TypeLiteral.get(clazz));
+
+                Collection<E> result = new ArrayList<E>(bindings.size());
+                for (Binding<E> binding : bindings) {
+                    result.add(binding.getProvider().get());
+                }
+                return result;
+
+            }
+
         }
-        return result;
+
+        return null;
+
     }
 
 }
