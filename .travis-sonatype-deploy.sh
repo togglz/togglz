945a4d94df3d2f5b2f78357e389b6891087f1c53
Christian Kaltepoth <christian@kaltepoth.de>
2014-05-22 17:44:14 +0200

    Travis: Updated snapshot deployment config

--- .travis-sonatype-deploy.sh
+++ .travis-sonatype-deploy.sh
@@ -3,11 +3,11 @@
 echo "====================[ Snapshot deployer ]=========================="
 
 IS_JDK7=`test $(java -version 2>&1 | fgrep -c '1.7.0') -gt 0; echo $?`
-IS_AS7=`test "$CONTAINER" = 'jboss-as-managed'; echo $?`
+IS_AS7=`test "$CONTAINER" = 'wildfly-managed'; echo $?`
 IS_SECURE=`test "$TRAVIS_SECURE_ENV_VARS" = 'true'; echo $?`
 
 echo "Built against JDK7:         $IS_JDK7"
-echo "JBoss AS7 profile active:   $IS_AS7"
+echo "Wildfly profile active:     $IS_AS7"
 echo "Secure variables available: $IS_SECURE"
 
 if [ $IS_JDK7 -eq 0 -a $IS_AS7 -eq 0 -a $IS_SECURE -eq 0  ]; then
