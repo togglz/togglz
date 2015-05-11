7a0fb20b9039547a8120f4c8c6b80eabb071ad08
Tappe <tappe@transdata.net>
2015-05-11 08:29:18 +0200

    Fixed whitespaces.

--- core/src/main/java/org/togglz/core/util/NamedFeature.java
+++ core/src/main/java/org/togglz/core/util/NamedFeature.java
@@ -7,23 +7,23 @@ import org.togglz.core.Feature;
 /**
  * This class can be used if just the name of a feature is known but not the enum type. It is similar to {@link UntypedFeature}
  * but doesn't try to lazily resolve the type when calling {@link #name()}.
- *
+ * 
  * @author Christian Kaltepoth
  */
 public class NamedFeature implements Feature, Serializable {
 
 	private static final long serialVersionUID = 7344455581363755625L;
 
-	private final String name;
+    private final String name;
 
-	public NamedFeature(String name) {
-		this.name = name;
-	}
+    public NamedFeature(String name) {
+        this.name = name;
+    }
 
-	@Override
-	public String name() {
-		return name;
-	}
+    @Override
+    public String name() {
+        return name;
+    }
 
 	@Override
 	public int hashCode() {
