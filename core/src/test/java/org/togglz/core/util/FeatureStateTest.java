611096ae7105f5b0fe85a18d5021896ad3f57b47
Christian Kaltepoth <christian@kaltepoth.de>
2015-05-12 08:34:53 +0200

    Code formatting...

--- core/src/test/java/org/togglz/core/util/FeatureStateTest.java
+++ core/src/test/java/org/togglz/core/util/FeatureStateTest.java
@@ -17,41 +17,45 @@ import org.togglz.core.repository.FeatureState;
 public class FeatureStateTest {
 
     private static final String FEATURE_NAME = "myFeature";
-	private static final String STRATEGYID = "myStrategy";
-	private static final String PARAM_NAME = "myParam";
-	private static final String PARAM_VALUE = "myParamValue";
+    private static final String STRATEGYID = "myStrategy";
+    private static final String PARAM_NAME = "myParam";
+    private static final String PARAM_VALUE = "myParamValue";
 
-	@Test
+    @Test
     public void serializable() throws IOException, ClassNotFoundException {
-    	NamedFeature feature = new NamedFeature(FEATURE_NAME);
-    	FeatureState featureState = new FeatureState(feature);
-    	featureState.setStrategyId(STRATEGYID);
-    	featureState.setParameter(PARAM_NAME, PARAM_VALUE);
-    	featureState.setEnabled(true);
-    	ByteArrayOutputStream os = new ByteArrayOutputStream();
-		ObjectOutputStream oos = new ObjectOutputStream(os);
-		try {
-			try {
-				oos.writeObject(featureState);
-				InputStream is = new ByteArrayInputStream(os.toByteArray());
-				ObjectInputStream ois = new ObjectInputStream(is);
-				try {
-					FeatureState deserializedFeatureState = (FeatureState) ois.readObject();
-					assertEquals("FeatureState.feature was not correctly serialized/deserialized", featureState.getFeature(), deserializedFeatureState.getFeature());
-					assertEquals("FeatureState.strategyId was not correctly serialized/deserialized", featureState.getStrategyId(), deserializedFeatureState.getStrategyId());
-					assertEquals("FeatureState.parameters were not correctly serialized/deserialized", featureState.getParameter(PARAM_NAME), deserializedFeatureState.getParameter(PARAM_NAME));
-					assertEquals("FeatureState.enabled was not correctly serialized/deserialized", featureState.isEnabled(), deserializedFeatureState.isEnabled());
-				} finally {
-					is.close();
-					ois.close();
-				}
-			} catch (NotSerializableException e) {
-				fail("FeatureState is not serializable");
-			}
-		} finally {
-			os.close();
-			oos.close();
-		}
+        NamedFeature feature = new NamedFeature(FEATURE_NAME);
+        FeatureState featureState = new FeatureState(feature);
+        featureState.setStrategyId(STRATEGYID);
+        featureState.setParameter(PARAM_NAME, PARAM_VALUE);
+        featureState.setEnabled(true);
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(os);
+        try {
+            try {
+                oos.writeObject(featureState);
+                InputStream is = new ByteArrayInputStream(os.toByteArray());
+                ObjectInputStream ois = new ObjectInputStream(is);
+                try {
+                    FeatureState deserializedFeatureState = (FeatureState) ois.readObject();
+                    assertEquals("FeatureState.feature was not correctly serialized/deserialized",
+                        featureState.getFeature(), deserializedFeatureState.getFeature());
+                    assertEquals("FeatureState.strategyId was not correctly serialized/deserialized",
+                        featureState.getStrategyId(), deserializedFeatureState.getStrategyId());
+                    assertEquals("FeatureState.parameters were not correctly serialized/deserialized",
+                        featureState.getParameter(PARAM_NAME), deserializedFeatureState.getParameter(PARAM_NAME));
+                    assertEquals("FeatureState.enabled was not correctly serialized/deserialized",
+                        featureState.isEnabled(), deserializedFeatureState.isEnabled());
+                } finally {
+                    is.close();
+                    ois.close();
+                }
+            } catch (NotSerializableException e) {
+                fail("FeatureState is not serializable");
+            }
+        } finally {
+            os.close();
+            oos.close();
+        }
     }
 
 }
--- core/src/test/java/org/togglz/core/util/NamedFeatureTest.java
+++ core/src/test/java/org/togglz/core/util/NamedFeatureTest.java
@@ -17,28 +17,29 @@ public class NamedFeatureTest {
 
     @Test
     public void serializable() throws IOException, ClassNotFoundException {
-    	NamedFeature feature = new NamedFeature("SERIALIZABLE");
-    	ByteArrayOutputStream os = new ByteArrayOutputStream();
-		ObjectOutputStream oos = new ObjectOutputStream(os);
-		try {
-			try {
-				oos.writeObject(feature);
-				InputStream is = new ByteArrayInputStream(os.toByteArray());
-				ObjectInputStream ois = new ObjectInputStream(is);
-				try {
-					Object deserializedFeature = ois.readObject();
-					assertEquals("NamedFeature was not correctly serialized/deserialized", feature, deserializedFeature);
-				} finally {
-					is.close();
-					ois.close();
-				}
-			} catch (NotSerializableException e) {
-				fail("NamedFeature is not serializable");
-			}
-		} finally {
-			os.close();
-			oos.close();
-		}
+        NamedFeature feature = new NamedFeature("SERIALIZABLE");
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(os);
+        try {
+            try {
+                oos.writeObject(feature);
+                InputStream is = new ByteArrayInputStream(os.toByteArray());
+                ObjectInputStream ois = new ObjectInputStream(is);
+                try {
+                    Object deserializedFeature = ois.readObject();
+                    assertEquals("NamedFeature was not correctly serialized/deserialized",
+                        feature, deserializedFeature);
+                } finally {
+                    is.close();
+                    ois.close();
+                }
+            } catch (NotSerializableException e) {
+                fail("NamedFeature is not serializable");
+            }
+        } finally {
+            os.close();
+            oos.close();
+        }
     }
 
 }
