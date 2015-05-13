abbe21676f5fe610c7e08f9f28b9997cfa9a6892
Christian Kaltepoth <christian@kaltepoth.de>
2015-05-13 17:13:05 +0200

    Fixed ordering issue in VariationSetBuilder (#131, #132)

--- junit/src/test/java/org/togglz/junit/vary/TogglzRuleWithVariations_CalledMultipleTimes_Test.java
+++ junit/src/test/java/org/togglz/junit/vary/TogglzRuleWithVariations_CalledMultipleTimes_Test.java
@@ -1,97 +1,55 @@
 package org.togglz.junit.vary;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.togglz.junit.TogglzRule;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class TogglzRuleWithVariations_CalledMultipleTimes_Test {
 
     @Rule
     public TogglzRule togglzRule = TogglzRule.builder(MyFeatures.class)
-            .vary(MyFeatures.F2)
-            .vary(MyFeatures.F3)
-            .build();
+        .vary(MyFeatures.F2)
+        .vary(MyFeatures.F3)
+        .build();
 
-    private List<Tuple> tuples = new ArrayList<Tuple>();
-    private int i;
+    private int counter;
 
     @Test
     public void test() {
-        tuples.add(new Tuple(MyFeatures.F1.isActive(), MyFeatures.F2.isActive(), MyFeatures.F3.isActive()));
 
-        i++;
-        switch (i) {
-        case 1:
-            // can't assert here because the order seems to vary between "environment" (ie mvn vs IDE)
-            break;
-        case 2:
-            // can't assert here because the order seems to vary between "environment" (ie mvn vs IDE)
-            break;
-        case 3:
-            // can't assert here because the order seems to vary between "environment" (ie mvn vs IDE)
-            break;
-        case 4:
-            assertTrue(tuples.contains(new Tuple(false,false,false)));
-            assertTrue(tuples.contains(new Tuple(false,false,true)));
-            assertTrue(tuples.contains(new Tuple(false,true,false)));
-            assertTrue(tuples.contains(new Tuple(false, true, true)));
-            break;
-        default:
-            fail();
+        counter++;
+
+        switch (counter) {
+            case 1:
+                assertEquals(false, MyFeatures.F1.isActive());
+                assertEquals(false, MyFeatures.F2.isActive());
+                assertEquals(false, MyFeatures.F3.isActive());
+                break;
+
+            case 2:
+                assertEquals(false, MyFeatures.F1.isActive());
+                assertEquals(true, MyFeatures.F2.isActive());
+                assertEquals(false, MyFeatures.F3.isActive());
+                break;
+            case 3:
+                assertEquals(false, MyFeatures.F1.isActive());
+                assertEquals(false, MyFeatures.F2.isActive());
+                assertEquals(true, MyFeatures.F3.isActive());
+                break;
+
+            case 4:
+                assertEquals(false, MyFeatures.F1.isActive());
+                assertEquals(true, MyFeatures.F2.isActive());
+                assertEquals(true, MyFeatures.F3.isActive());
+                break;
+
+            default:
+                fail("Incorrect execution cound");
+
         }
     }
-
-
-    static class Tuple {
-        boolean f1;
-        boolean f2;
-        boolean f3;
-
-        public Tuple(final boolean f1, final boolean f2, final boolean f3) {
-            this.f1 = f1;
-            this.f2 = f2;
-            this.f3 = f3;
-        }
-
-        @Override
-        public boolean equals(final Object o) {
-            if (this == o)
-                return true;
-            if (o == null || getClass() != o.getClass())
-                return false;
-
-            final Tuple tuple = (Tuple) o;
-
-            if (f1 != tuple.f1)
-                return false;
-            if (f2 != tuple.f2)
-                return false;
-            return f3 == tuple.f3;
-        }
-
-        @Override
-        public int hashCode() {
-            int result = (f1 ? 1 : 0);
-            result = 31 * result + (f2 ? 1 : 0);
-            result = 31 * result + (f3 ? 1 : 0);
-            return result;
-        }
-
-        @Override public String toString() {
-            return "Tuple{" +
-                    "f1=" + f1 +
-                    ", f2=" + f2 +
-                    ", f3=" + f3 +
-                    '}';
-        }
-    }
-
-
 
 }
