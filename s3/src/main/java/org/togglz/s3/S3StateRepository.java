5a893a114e6bc38f4b941a27e5f28a28d4ce1b1e
MarkRx <MarkRx@users.noreply.github.com>
2016-12-27 13:40:09 -0500

    Add Amazon s3 implementation

---
+++ s3/src/main/java/org/togglz/s3/S3StateRepository.java
@@ -0,0 +1,120 @@
+package org.togglz.s3;
+
+import org.togglz.core.Feature;
+import org.togglz.core.repository.FeatureState;
+import org.togglz.core.repository.StateRepository;
+import org.togglz.core.util.FeatureStateStorageWrapper;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.S3Object;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * A state repository that uses Amazon S3.
+ * <p>
+ * The repository is configured using the {@link Builder}
+ * <p>
+ * You must already have a bucket provisioned before you create this repository.
+ * 
+ * @author Mark Richardson
+ * @date 12/27/16
+ */
+public class S3StateRepository implements StateRepository {
+	
+	// http://docs.aws.amazon.com/AmazonS3/latest/API/ErrorResponses.html
+	private static final String ERR_NO_SUCH_KEY = "NoSuchKey";
+	
+	private ObjectMapper objectMapper = new ObjectMapper();
+	
+	protected final AmazonS3Client client;
+	protected final String bucketName;
+	protected final String keyPrefix;
+	
+	private S3StateRepository(Builder builder) {
+		this.client = builder.client;
+		this.bucketName = builder.bucketName;
+		this.keyPrefix = builder.keyPrefix;
+	}
+
+	@Override
+	public FeatureState getFeatureState(Feature feature) {
+		try (S3Object object = client.getObject(bucketName, keyPrefix + feature.name())) {
+			if (object != null) {
+				FeatureStateStorageWrapper wrapper = objectMapper.reader().forType(FeatureStateStorageWrapper.class).readValue(object.getObjectContent());
+				return FeatureStateStorageWrapper.featureStateForWrapper(feature, wrapper);
+			}
+		} catch (AmazonS3Exception ae) {
+			if (ERR_NO_SUCH_KEY.equals(ae.getErrorCode())) {
+				return null;
+			}
+			throw ae;
+		} catch (Exception e) {
+			 throw new RuntimeException("Failed to get the feature state", e);
+		}
+		
+		return null;
+	}
+
+	@Override
+	public void setFeatureState(FeatureState featureState) {
+		try {
+			String json = objectMapper.writeValueAsString(FeatureStateStorageWrapper.wrapperForFeatureState(featureState));
+			
+			client.putObject(bucketName, keyPrefix + featureState.getFeature().name(), json);
+		} catch (Exception e) {
+            throw new RuntimeException("Failed to set the feature state", e);
+        }
+	}
+	
+    /**
+     * Creates a new builder for creating a {@link S3StateRepository}.
+     *
+     * @param client the client instance to use for connecting to amazon s3
+     * @param bucketName The name of the bucket to use
+     * @return a Builder
+     */
+    public static Builder newBuilder(AmazonS3Client client, String bucketName) {
+        return new Builder(client, bucketName);
+    }
+	
+    /**
+     * Builder for a {@link S3StateRepository}.
+     */
+    public static class Builder {
+
+        private final AmazonS3Client client;
+        private final String bucketName;
+        private String keyPrefix = "togglz/";
+
+        /**
+         * Creates a new builder for a {@link S3StateRepository}.
+         *
+         * @param client the client instance to use for connecting to amazon s3
+         * @param bucketName The name of the bucket to use
+         */
+        public Builder(AmazonS3Client client, String bucketName) {
+            this.client = client;
+            this.bucketName = bucketName;
+        }
+        
+        /**
+         * Optional prefixes to prepend on to each key
+         * 
+         * @param prefix The prefix to use
+         * @return
+         */
+        public Builder prefix(String keyPrefix) {
+        	this.keyPrefix = keyPrefix == null? "" : keyPrefix;
+        	return this;
+        }
+
+        /**
+         * Creates a new {@link MongoStateRepository} using the current settings.
+         */
+        public S3StateRepository build() {
+            return new S3StateRepository(this);
+        }
+
+    }
+}
---
+++ s3/src/test/java/org/togglz/s3/S3StateRepositoryTest.java
@@ -0,0 +1,102 @@
+package org.togglz.s3;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertNull;
+
+import java.io.ByteArrayInputStream;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+import org.junit.Test;
+import org.togglz.core.Feature;
+import org.togglz.core.repository.FeatureState;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AnonymousAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.PutObjectResult;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+
+public class S3StateRepositoryTest {
+	@SuppressWarnings("serial")
+	@Test
+	public void test() {
+		AmazonS3Client client = new AmazonS3ClientMOCK(new AnonymousAWSCredentials());
+		client.withEndpoint(String.format("http://localhost:8001"));
+		client.createBucket("testbucket");
+
+		S3StateRepository repository = S3StateRepository.newBuilder(client, "testbucket").build();
+
+		assertNull(repository.getFeatureState(TestFeature.FEATURE_1));
+
+		FeatureState initFs = new FeatureState(TestFeature.FEATURE_1)
+				.setEnabled(true)
+				.setStrategyId("abc")
+				.setParameter("key1", "value1");
+
+		repository.setFeatureState(initFs);
+
+		FeatureState actualFs = repository.getFeatureState(TestFeature.FEATURE_1);
+
+		assertThat(actualFs.getFeature()).isEqualTo(initFs.getFeature());
+		assertThat(actualFs.getStrategyId()).isEqualTo("abc");
+		assertThat(actualFs.isEnabled()).isEqualTo(true);
+		assertThat(actualFs.getParameter("key1")).isEqualTo("value1");
+		assertThat(actualFs.getParameterNames()).isEqualTo(new HashSet<String>() {{ add("key1"); }});
+		
+		FeatureState updatedFs = new FeatureState(TestFeature.FEATURE_1)
+				.setEnabled(false)
+				.setStrategyId("def")
+				.setParameter("key2", "value2");
+
+		repository.setFeatureState(updatedFs);
+		
+		actualFs = repository.getFeatureState(TestFeature.FEATURE_1);
+		
+		assertThat(actualFs.getFeature()).isEqualTo(initFs.getFeature());
+		assertThat(actualFs.getStrategyId()).isEqualTo("def");
+		assertThat(actualFs.isEnabled()).isEqualTo(false);
+		assertThat(actualFs.getParameter("key2")).isEqualTo("value2");
+		assertThat(actualFs.getParameterNames()).isEqualTo(new HashSet<String>() {{ add("key2"); }});
+	}
+	
+    private enum TestFeature implements Feature {
+        FEATURE_1
+    }
+    
+    private static class AmazonS3ClientMOCK extends AmazonS3Client {
+    	Map<String, Map<String, S3Object>> repo = new HashMap<String, Map<String, S3Object>>();
+    	
+    	public AmazonS3ClientMOCK(AWSCredentials awsCredentials) {
+    		super(awsCredentials);
+    	}
+    	
+    	@Override
+    	public S3Object getObject(String bucketName, String key) {
+    		return repo.get(bucketName).get(key);
+    	}
+    	
+    	@Override
+    	public PutObjectResult putObject(String bucketName, String key, String content) {
+    		Map<String, S3Object> r2 = repo.get(bucketName);
+    		
+    		ByteArrayInputStream in = new ByteArrayInputStream(content.getBytes());
+    		S3Object s3obj = new S3Object();
+    		s3obj.setObjectContent(new S3ObjectInputStream(in, null));
+    		
+    		r2.put(key, s3obj);
+    		
+    		return new PutObjectResult();
+    	}
+    	
+    	@Override
+    	public Bucket createBucket(String bucketName) {
+    		repo.put(bucketName, new HashMap<String, S3Object>());
+    		
+			return new Bucket();
+    	}
+    }
+}
