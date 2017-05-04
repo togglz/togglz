fdb8f630347bdfcb79492dde93d5043ebfccdc53
Cosmin Rentea <c.rentea@tarent.de>
2017-05-04 14:50:05 +0300

    Zookeeper: upgrading dependencies

--- zookeeper/src/test/java/org/togglz/zookeeper/ZookeeperStateRepositoryTest.java
+++ zookeeper/src/test/java/org/togglz/zookeeper/ZookeeperStateRepositoryTest.java
@@ -20,7 +20,7 @@ import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
-import static org.apache.commons.lang.builder.EqualsBuilder.reflectionEquals;
+import static org.apache.commons.lang3.builder.EqualsBuilder.reflectionEquals;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
