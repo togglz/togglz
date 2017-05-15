25b60d4a7e1c73079317b583a4b9bce3052076cc
Cosmin Rentea <c.rentea@tarent.de>
2017-05-15 12:40:45 +0300

    ClientActivationStrategyTest: added 3 more tests: one for X-Forwarded-For header, two for isValid() of multiple addresses/ranges; cleanup; formatting

--- servlet/src/test/java/org/togglz/servlet/activation/ClientIpActivationStrategyTest.java
+++ servlet/src/test/java/org/togglz/servlet/activation/ClientIpActivationStrategyTest.java
@@ -1,13 +1,5 @@
 package org.togglz.servlet.activation;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import static org.togglz.servlet.activation.ClientIpActivationStrategyTest.MockRequest.requestFrom;
-import static org.togglz.servlet.activation.ClientIpActivationStrategyTest.MockRequestAssert.assertThat;
-
 import org.assertj.core.api.Assertions;
 import org.junit.After;
 import org.junit.Test;
@@ -18,66 +10,84 @@ import org.togglz.servlet.util.HttpServletRequestHolder;
 
 import javax.servlet.http.HttpServletRequest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.togglz.servlet.activation.ClientIpActivationStrategyTest.MockRequest.requestFrom;
+import static org.togglz.servlet.activation.ClientIpActivationStrategyTest.MockRequest.requestWithHeader;
+import static org.togglz.servlet.activation.ClientIpActivationStrategyTest.MockRequestAssert.assertThat;
+
 public class ClientIpActivationStrategyTest {
 
-   protected static class MockRequest {
-      private final HttpServletRequest request;
-      
-      public static MockRequest requestFrom(String remoteAddr) {
-         return new MockRequest(remoteAddr);
-      }
-      
-      private MockRequest(String remoteAddr) {
-         request = mock(HttpServletRequest.class);
-         when(request.getRemoteAddr()).thenReturn(remoteAddr);
-         HttpServletRequestHolder.bind(request);
-      }
-      
-      public HttpServletRequest getRequest() {
-         return request;
-      }
-   }
+    protected static class MockRequest {
+        private final HttpServletRequest request;
 
-   protected static class MockRequestAssert extends org.assertj.core.api.AbstractAssert<MockRequestAssert, MockRequest> {
-      protected MockRequestAssert(MockRequest actual) {
-         super(actual, MockRequestAssert.class);
-      }
-      
-      public static MockRequestAssert assertThat(MockRequest actual) {
-         return new MockRequestAssert(actual);
-      }
-      
-      public MockRequestAssert isActiveWithParams(String params) {
-         if (!strategy().isActive(featureState(params), null)) {
-            Assertions.fail("Expected the strategy to turn the feature active with params " + params);
-         }
-         return this;
-      }
+        private MockRequest(final String remoteAddr) {
+            request = mock(HttpServletRequest.class);
+            when(request.getRemoteAddr()).thenReturn(remoteAddr);
+            HttpServletRequestHolder.bind(request);
+        }
 
-      public MockRequestAssert isInactiveWithParams(String params) {
-         if (strategy().isActive(featureState(params), null)) {
-            Assertions.fail("Expected the strategy to turn the feature inactive with params " + params);
-         }
-         return this;
-      }
+        private MockRequest(final String header, final String headerValue) {
+            request = mock(HttpServletRequest.class);
+            when(request.getHeader(header)).thenReturn(headerValue);
+            HttpServletRequestHolder.bind(request);
+        }
 
-      private static ClientIpActivationStrategy strategy() {
-          return new ClientIpActivationStrategy();
-      }
-      
+        public static MockRequest requestFrom(final String remoteAddr) {
+            return new MockRequest(remoteAddr);
+        }
 
-      private static FeatureState featureState(String ips) {
-          return new FeatureState(TestFeature.TEST_FEATURE)
-              .enable()
-              .setStrategyId(ClientIpActivationStrategy.ID)
-              .setParameter(ClientIpActivationStrategy.PARAM_IPS, ips);
-      }
+        public static MockRequest requestWithHeader(final String header, final String headerValue) {
+            return new MockRequest(header, headerValue);
+        }
 
-      private enum TestFeature implements Feature {
-          TEST_FEATURE
-      }
-   }
-   
+        public HttpServletRequest getRequest() {
+            return request;
+        }
+    }
+
+    protected static class MockRequestAssert extends org.assertj.core.api.AbstractAssert<MockRequestAssert, MockRequest> {
+        protected MockRequestAssert(MockRequest actual) {
+            super(actual, MockRequestAssert.class);
+        }
+
+        public static MockRequestAssert assertThat(MockRequest actual) {
+            return new MockRequestAssert(actual);
+        }
+
+        private static ClientIpActivationStrategy strategy() {
+            return new ClientIpActivationStrategy();
+        }
+
+        private static FeatureState featureState(String ips) {
+            return new FeatureState(TestFeature.TEST_FEATURE)
+                    .enable()
+                    .setStrategyId(ClientIpActivationStrategy.ID)
+                    .setParameter(ClientIpActivationStrategy.PARAM_IPS, ips);
+        }
+
+        public MockRequestAssert isActiveWithParams(String params) {
+            if (!strategy().isActive(featureState(params), null)) {
+                Assertions.fail("Expected the strategy to turn the feature active with params " + params);
+            }
+            return this;
+        }
+
+        public MockRequestAssert isInactiveWithParams(String params) {
+            if (strategy().isActive(featureState(params), null)) {
+                Assertions.fail("Expected the strategy to turn the feature inactive with params " + params);
+            }
+            return this;
+        }
+
+        private enum TestFeature implements Feature {
+            TEST_FEATURE
+        }
+    }
+
     @After
     public void cleanup() {
         HttpServletRequestHolder.release();
@@ -85,12 +95,12 @@ public class ClientIpActivationStrategyTest {
 
     @Test
     public void shouldBeInactiveForNullParams() throws Exception {
-       assertThat(requestFrom("10.1.2.3")).isInactiveWithParams(null);
+        assertThat(requestFrom("10.1.2.3")).isInactiveWithParams(null);
     }
-    
+
     @Test
     public void shouldBeInactiveForEmptyParams() throws Exception {
-       assertThat(requestFrom("10.1.2.3")).isInactiveWithParams("");
+        assertThat(requestFrom("10.1.2.3")).isInactiveWithParams("");
     }
 
     @Test
@@ -130,84 +140,100 @@ public class ClientIpActivationStrategyTest {
 
     @Test
     public void shouldBeInactiveForNonMatchingIpv6() throws Exception {
-       assertThat(requestFrom("2001:db8:0:0:0:0:0:1")).isInactiveWithParams("2001:db8:0:0:0:0:0:2");
+        assertThat(requestFrom("2001:db8:0:0:0:0:0:1")).isInactiveWithParams("2001:db8:0:0:0:0:0:2");
     }
-    
+
     @Test
     public void shouldBeActiveForMatchingIpv6() throws Exception {
-       assertThat(requestFrom("2001:db8:0:0:0:0:0:1")).isActiveWithParams("2001:db8:0:0:0:0:0:1");
+        assertThat(requestFrom("2001:db8:0:0:0:0:0:1")).isActiveWithParams("2001:db8:0:0:0:0:0:1");
     }
 
     @Test
     public void shouldBeInactiveForNonMatchingIpv6ShortForm() throws Exception {
-       assertThat(requestFrom("2001:db8:0:0:0:0:0:1")).isInactiveWithParams("2001:db8::2");
+        assertThat(requestFrom("2001:db8:0:0:0:0:0:1")).isInactiveWithParams("2001:db8::2");
     }
-    
+
     @Test
     public void shouldBeActiveForMatchingIpv6ShortForm() throws Exception {
-       assertThat(requestFrom("2001:db8:0:0:0:0:0:1")).isActiveWithParams("2001:db8::1");
+        assertThat(requestFrom("2001:db8:0:0:0:0:0:1")).isActiveWithParams("2001:db8::1");
     }
 
     @Test
     public void shouldBeActiveForMatchingIpv6Range() throws Exception {
-       assertThat(requestFrom("2001:db8:0:0:0:0:0:1")).isActiveWithParams("2001:db8::/24");
+        assertThat(requestFrom("2001:db8:0:0:0:0:0:1")).isActiveWithParams("2001:db8::/24");
     }
-    
+
+    @Test
+    public void shouldBeActiveForHeaderXForwardedFor() {
+        assertThat(requestWithHeader("X-Forwarded-For", "192.168.0.1")).isActiveWithParams("192.168.0.1,10.1.2.3");
+    }
+
     @Test
     public void addressParameterProperties() {
-       AddressParameter param = addressParam();
-       assertThat(param.getDescription()).isNotEmpty();
-       assertThat(param.getLabel()).isNotEmpty();
-       assertThat(param.getName()).isEqualTo(ClientIpActivationStrategy.PARAM_IPS);
-       assertThat(param.isLargeText()).isFalse();
-       assertThat(param.isOptional()).isFalse();
+        final AddressParameter param = addressParam();
+        assertThat(param.getDescription()).isNotEmpty();
+        assertThat(param.getLabel()).isNotEmpty();
+        assertThat(param.getName()).isEqualTo(ClientIpActivationStrategy.PARAM_IPS);
+        assertThat(param.isLargeText()).isFalse();
+        assertThat(param.isOptional()).isFalse();
     }
 
     @Test
     public void addressParameterShouldBeInvalidWithEmptyInput() {
-       assertFalse(addressParam().isValid(null));
-       assertFalse(addressParam().isValid(""));
+        assertFalse(addressParam().isValid(null));
+        assertFalse(addressParam().isValid(""));
     }
 
     @Test
     public void addressParameterShouldBeInvalidWithWrongCidrFormat() {
-       assertFalse(addressParam().isValid("some-invalid-string/24"));
+        assertFalse(addressParam().isValid("some-invalid-string/24"));
     }
 
     @Test
     public void addressParameterShouldBeValidWithIpv4Address() {
-       assertTrue(addressParam().isValid("10.1.2.3"));
+        assertTrue(addressParam().isValid("10.1.2.3"));
     }
 
     @Test
     public void addressParameterShouldBeInvalidWithWrongIpv4Address() {
-       assertFalse(addressParam().isValid("1.2....."));
+        assertFalse(addressParam().isValid("1.2....."));
     }
 
     @Test
     public void addressParameterShouldBeInvalidWithWrongIpv6Address() {
-       assertFalse(addressParam().isValid("[2001:db8::1"));
-       assertFalse(addressParam().isValid("2001:db8:::1"));
+        assertFalse(addressParam().isValid("[2001:db8::1"));
+        assertFalse(addressParam().isValid("2001:db8:::1"));
     }
 
     @Test
     public void addressParameterShouldBeValidWithIpv4AddressRange() {
-       assertTrue(addressParam().isValid("10.1.2.0/24"));
+        assertTrue(addressParam().isValid("10.1.2.0/24"));
     }
 
     @Test
     public void addressParameterShouldBeValidWithIpv6Address() {
-       assertTrue(addressParam().isValid("2001:db8:0:0:0:0:0:1"));
-       assertTrue(addressParam().isValid("2001:db8::1"));
+        assertTrue(addressParam().isValid("2001:db8:0:0:0:0:0:1"));
+        assertTrue(addressParam().isValid("2001:db8::1"));
     }
 
     @Test
     public void addressParameterShouldBeValidWithIpv6AddressRange() {
-       assertTrue(addressParam().isValid("2001:db8:0:0:0:0:0:0/24"));
-       assertTrue(addressParam().isValid("2001:db8::/24"));
+        assertTrue(addressParam().isValid("2001:db8:0:0:0:0:0:0/24"));
+        assertTrue(addressParam().isValid("2001:db8::/24"));
+    }
+
+    @Test
+    public void addressParameterShouldBeValidWithMoreAddressesAndRanges() {
+        assertTrue(addressParam().isValid("10.1.2.3,10.1.2.0/24,2001:db8:0:0:0:0:0:1,2001:db8::1,2001:db8:0:0:0:0:0:0/24,2001:db8::/24"));
+    }
+
+    @Test
+    public void addressParameterShouldBeInvalidWithMoreAddressesAndRangesAndOneInvalid() {
+        assertFalse(addressParam().isValid("10.1.2.3,10.1.2.0/24,2001:db8:0:0:0:0:0:1,2001:db8::1,2001:db8:0:0:0:0:0:0/24,2001:db8::/24,some-invalid-string/24"));
+        assertFalse(addressParam().isValid("10.1.2.3,10.1.2.0/24,2001:db8:0:0:0:0:0:1,2001:db8:::1,2001:db8:0:0:0:0:0:0/24,2001:db8::/24"));
     }
 
     private AddressParameter addressParam() {
-       return new AddressParameter();
+        return new AddressParameter();
     }
 }
