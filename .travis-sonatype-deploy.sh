f1900f35bb1064fe59d68f65dab8c6560ea7a310
Christian Kaltepoth <christian@kaltepoth.de>
2018-02-11 12:33:19 +0100

    Updated snapshot script to deploy the JDK8 version

--- .travis-sonatype-deploy.sh
+++ .travis-sonatype-deploy.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+set -euo pipefail
+
 echo "====================[ Snapshot deployer ]=========================="
 
-IS_JDK7=`test $(java -version 2>&1 | fgrep -c '1.7.0') -gt 0; echo $?`
+IS_JDK8=`test $(java -version 2>&1 | fgrep -c '1.8.0') -gt 0; echo $?`
 IS_WILDFLY=`test "$CONTAINER" = 'wildfly-managed'; echo $?`
 IS_SECURE=`test "$TRAVIS_SECURE_ENV_VARS" = 'true'; echo $?`
 
-echo "Built against JDK7:         $IS_JDK7"
+echo "Built against JDK8:         $IS_JDK8"
 echo "Wildfly profile active:     $IS_WILDFLY"
 echo "Secure variables available: $IS_SECURE"
 
-if [ $IS_JDK7 -eq 0 -a $IS_WILDFLY -eq 0 -a $IS_SECURE -eq 0  ]; then
+if [ $IS_JDK8 -eq 0 -a $IS_WILDFLY -eq 0 -a $IS_SECURE -eq 0  ]; then
   mvn -s .travis-sonatype-settings.xml -DperformRelease -DskipTests -DJBOSS_REPO=true deploy
 else
   echo "Not deploying snapshots"
