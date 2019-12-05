ebcb32d3c69900106c325a14a3b06f7d32cdca67
Bennet Schulz <mail@bennet-schulz.de>
2019-12-05 12:07:21 +0100

    add documentation for togglz actuator endpoint (#356)
    * add documentation for togglz actuator endpoint
    
    * extract method

--- spring-boot/starter/src/main/java/org/togglz/spring/boot/actuate/TogglzEndpoint.java
+++ spring-boot/starter/src/main/java/org/togglz/spring/boot/actuate/TogglzEndpoint.java
@@ -31,7 +31,10 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * Spring Boot 2+ {@link Endpoint} to expose Togglz info.
+ * Spring Boot 2+ {@link Endpoint} to expose Togglz info as an actuator endpoint.
+ * This endpoint is exposed as Spring Boot Actuator endpoint. It allows the user to get an overview of
+ * all available toggles without adding the togglz-console dependency.
+ * The user can also read the state of specific toggles and even enable or disable specific toggles.
  *
  * @author Rui Figueira
  */
@@ -56,17 +59,28 @@ public class TogglzEndpoint  {
         return features;
     }
 
+    /**
+     * Allows to change the state of toggles via http post.
+     *
+     * @param name the name of the toggle/feature
+     * @param enabled the name of the field containing the toggle/feature status
+     */
     @WriteOperation
-    public TogglzFeature setFeatureState(@Selector String featureName, boolean enabled) {
+    public TogglzFeature setFeatureState(@Selector String name, boolean enabled) {
         final Feature feature = featureManager.getFeatures().stream()
-                .filter(f -> f.name().equals(featureName))
+                .filter(f -> f.name().equals(name))
                 .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("Could not find feature with name " + featureName));
+                .orElseThrow(() -> new IllegalArgumentException("Could not find feature with name " + name));
 
+        FeatureState featureState = changeFeatureStatus(feature, enabled);
+
+        return new TogglzFeature(feature, featureState);
+    }
+
+    private FeatureState changeFeatureStatus(Feature feature, boolean enabled) {
         FeatureState featureState = featureManager.getFeatureState(feature);
         featureState.setEnabled(enabled);
         featureManager.setFeatureState(featureState);
-
-        return new TogglzFeature(feature, featureState);
+        return featureState;
     }
 }
