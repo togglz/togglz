295dbc2970dc98b51b8c13ee037960649c7d1532
Bennet Schulz <mail@bennet-schulz.de>
2020-12-21 13:59:35 +0100

    replace oldschool cdi dependencies with the newer jakarta one (#465)

--- cloud-datastore/src/main/java/org/togglz/googleclouddatastore/repository/GoogleCloudDatastoreStateRepository.java
+++ cloud-datastore/src/main/java/org/togglz/googleclouddatastore/repository/GoogleCloudDatastoreStateRepository.java
@@ -5,12 +5,12 @@ import com.google.cloud.datastore.Entity;
 import com.google.cloud.datastore.Key;
 import com.google.cloud.datastore.KeyFactory;
 import com.google.cloud.datastore.Value;
+import jakarta.inject.Inject;
 import org.togglz.core.Feature;
 import org.togglz.core.repository.FeatureState;
 import org.togglz.core.repository.StateRepository;
 import org.togglz.core.util.Preconditions;
 
-import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
