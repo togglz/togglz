3a663997199a119354f77af4dc71ae68519d59a6
M Schröer <16744580+m-schroeer@users.noreply.github.com>
2021-01-26 14:41:15 +0100

    Issue #492 httpstatus (#496)
    * support #492 (use org.springframework.http.HttpStatus instead of 'int response')
    
    In order to not break current code FeaturesAreActive#responseStatus (1) is set as 'Deprecated'.
    FeaturesAreActive#errorResponseStatus (2) which returns org.springframework.http.HttpStatus was introduced.
    In order to support the deprecation following behaviour was implemented:
    - If (1) and (2) are equal -> fine, return it
    - If (1) and (2) are not equal but one is not the default value -> fine, return the non default value
    - If (1) and (2) are different and both differ from default value throw IllegalArgumentException
    
    * togglz-spring-web: replace deprecated HandlerInterceptorAdapter with HandlerInterceptor
    
    * togglz-spring-web: clean up

--- spring-web/src/main/java/org/togglz/spring/web/FeatureInterceptor.java
+++ spring-web/src/main/java/org/togglz/spring/web/FeatureInterceptor.java
@@ -1,24 +1,24 @@
 package org.togglz.spring.web;
 
 import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.method.HandlerMethod;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.togglz.core.Feature;
 import org.togglz.core.context.FeatureContext;
 import org.togglz.core.manager.FeatureManager;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.lang.annotation.Annotation;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 /**
  * This interceptor checks if a controller or controller method is annotated with the
- * {@link FeaturesAreActive} annotation to determine if a controller should be 
+ * {@link FeaturesAreActive} annotation to determine if a controller should be
  * activated or not.
  * <p>
  * Set the togglz.web.register-feature-interceptor to {@code true} to activate this interceptor.
@@ -26,80 +26,106 @@ import javax.servlet.http.HttpServletResponse;
  * @author ractive
  * @author m-schroeer
  */
-public class FeatureInterceptor extends HandlerInterceptorAdapter {
+public class FeatureInterceptor implements HandlerInterceptor {
+
     /**
      * Used to store annotations in a ConcurrentHashMap which does not allow storing {@code null} values.
-     * 
+     *
      * @param <A> annotation type
      */
     private static final class AnnotationHolder<A extends Annotation> {
+
         private final A annotation;
-        
-        public AnnotationHolder(A annotation) {
+
+        public AnnotationHolder(final A annotation) {
             this.annotation = annotation;
         }
-        
+
         public A getAnnotation() {
-            return annotation;
+            return this.annotation;
         }
-        
+
         public boolean hasAnnotation() {
-            return annotation != null;
+            return this.annotation != null;
         }
     }
 
     /**
      * Caches the annotations on the {@link HandlerMethod}s to avoid expensive reflection calls for every request.
      */
-    private ConcurrentHashMap<HandlerMethod, AnnotationHolder<FeaturesAreActive>> annotations = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<HandlerMethod, AnnotationHolder<FeaturesAreActive>> annotations = new ConcurrentHashMap<>();
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler) throws Exception {
         if (handler instanceof HandlerMethod) {
-            HandlerMethod handlerMethod = (HandlerMethod) handler;
-            AnnotationHolder<FeaturesAreActive> annotationHolder = annotations.get(handlerMethod);
+            final HandlerMethod handlerMethod = (HandlerMethod) handler;
+            AnnotationHolder<FeaturesAreActive> annotationHolder = this.annotations.get(handlerMethod);
             if (annotationHolder == null) {
-                FeaturesAreActive foundAnnotation = handlerAnnotation(handlerMethod, FeaturesAreActive.class);
-                annotations.putIfAbsent(handlerMethod, new AnnotationHolder<FeaturesAreActive>(foundAnnotation));
-                annotationHolder = annotations.get(handlerMethod);
+                final FeaturesAreActive foundAnnotation = handlerAnnotation(handlerMethod, FeaturesAreActive.class);
+                this.annotations.putIfAbsent(handlerMethod, new AnnotationHolder<>(foundAnnotation));
+                annotationHolder = this.annotations.get(handlerMethod);
             }
-            
-            if (annotationHolder.hasAnnotation()) {
-                FeaturesAreActive featuresAreActiveAnnotation = annotationHolder.getAnnotation();
 
-                Set<String> annotationFeatureNames = Stream.of(
+            if (annotationHolder.hasAnnotation()) {
+                final FeaturesAreActive featuresAreActiveAnnotation = annotationHolder.getAnnotation();
+
+                final Set<String> annotationFeatureNames = Stream.of(
                         featuresAreActiveAnnotation.features())
                         .collect(Collectors.toSet());
 
-                FeatureManager featureManager = FeatureContext.getFeatureManager();
+                final FeatureManager featureManager = FeatureContext.getFeatureManager();
 
                 if (!getFeatureNames(featureManager).containsAll(annotationFeatureNames)) {
                     throw new IllegalArgumentException("At least one given feature of '" + annotationFeatureNames + "' is not a feature!");
                 }
 
-                boolean allFeaturesOfAnnotationMatch = featureManager.getFeatures()
+                final boolean allFeaturesOfAnnotationMatch = featureManager.getFeatures()
                         .stream()
                         // reduce to features of annotation
                         .filter(feature -> annotationFeatureNames.contains(feature.name()))
                         .allMatch(featureManager::isActive);
 
                 if (!allFeaturesOfAnnotationMatch) {
-                    response.sendError(featuresAreActiveAnnotation.responseStatus());
+                    final int errorStatusCode = getErrorStatus(featuresAreActiveAnnotation).value();
+                    response.sendError(errorStatusCode);
                     return false;
                 }
             }
         }
-        return super.preHandle(request, response, handler);
+        return HandlerInterceptor.super.preHandle(request, response, handler);
     }
 
-    protected static <A extends Annotation> A handlerAnnotation(HandlerMethod handlerMethod, Class<A> annotationClass) {
+    // TODO: When deprecated field FeaturesAreActive#responseStatus is removed, this method could be removed as well and
+    //       be replaced by inline call to FeaturesAreActive#errorResponseStatus.
+    private HttpStatus getErrorStatus(final FeaturesAreActive annotation) {
+        final HttpStatus responseHttpStatus = HttpStatus.valueOf(annotation.responseStatus());
+
+        final HttpStatus errorResponseStatus = annotation.errorResponseStatus();
+
+        if (errorResponseStatus == responseHttpStatus) {
+            return errorResponseStatus;
+        } else {
+            // errorResponseStatus != responseHttpStatus
+            if (errorResponseStatus == FeaturesAreActive.DEFAULT_ERROR_RESPONSE_STATUS) {
+                // return the non-default value
+                return responseHttpStatus;
+            } else if (responseHttpStatus == FeaturesAreActive.DEFAULT_ERROR_RESPONSE_STATUS) {
+                // return the non-default value
+                return errorResponseStatus;
+            } else {
+                throw new IllegalArgumentException("'responseStatus' and 'errorResponseStatus' cannot be both non-default and different!");
+            }
+        }
+    }
+
+    protected static <A extends Annotation> A handlerAnnotation(final HandlerMethod handlerMethod, final Class<A> annotationClass) {
         A annotation = handlerMethod.getMethodAnnotation(annotationClass);
         if (annotation == null) {
             annotation = AnnotationUtils.findAnnotation(handlerMethod.getBeanType(), annotationClass);
         }
         return annotation;
     }
-    
+
     protected static Set<String> getFeatureNames(final FeatureManager featureManager) {
         return featureManager.getFeatures().stream()
                 .map(Feature::name)
