9fd9d16818ff71474c284029c7ddf21a2da92946
wim wintmolders <wim.wintmolders@joyn.be>
2021-02-12 10:57:09 +0100

    add setter for validateCSRFToken

--- spring-boot/starter/src/main/java/org/togglz/spring/boot/actuate/autoconfigure/TogglzProperties.java
+++ spring-boot/starter/src/main/java/org/togglz/spring/boot/actuate/autoconfigure/TogglzProperties.java
@@ -335,6 +335,10 @@ public class TogglzProperties {
 			return validateCSRFToken;
 		}
 
+		public void setValidateCSRFToken(boolean validateCSRFToken) {
+			this.validateCSRFToken = validateCSRFToken;
+		}
+
 		public boolean isEnabled() {
 			return enabled;
 		}
