bc405184579b5baaa18fb3ae3c2f465c3a464a91
Bennet Schulz <mail@bennet-schulz.de>
2021-04-23 08:31:44 +0200

    fix servlet filter conflict in war-packaged Spring Boot apps (#537)
    * fix servlet filter conflict in war-packaged Spring Boot apps
    
    * remove redundant configuration

--- spring-boot/starter/src/main/java/org/togglz/spring/boot/actuate/autoconfigure/TogglzAutoConfiguration.java
+++ spring-boot/starter/src/main/java/org/togglz/spring/boot/actuate/autoconfigure/TogglzAutoConfiguration.java
@@ -255,6 +255,7 @@ public class TogglzAutoConfiguration {
     @Configuration
     @ConditionalOnClass({OncePerRequestFilter.class})
     @ConditionalOnMissingBean(HttpServletRequestHolderFilter.class)
+    @ConditionalOnProperty(prefix = "togglz.web", name = "request-holder-filter", havingValue = "true")
     protected static class RequestHolderFilterConfiguration {
 
         @Bean
