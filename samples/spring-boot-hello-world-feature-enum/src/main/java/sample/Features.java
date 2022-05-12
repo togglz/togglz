e0c82a5a915b644290864802767fec732694862f
Bennet Schulz <mail@bennet-schulz.de>
2022-05-12 17:42:53 +0200

    Add FeatureGroups again

--- samples/spring-boot-hello-world-feature-enum/src/main/java/sample/Features.java
+++ samples/spring-boot-hello-world-feature-enum/src/main/java/sample/Features.java
@@ -2,19 +2,19 @@ package sample;
 
 import org.togglz.core.Feature;
 import org.togglz.core.annotation.EnabledByDefault;
-//import org.togglz.core.annotation.FeatureGroup;
+import org.togglz.core.annotation.FeatureGroup;
 import org.togglz.core.annotation.Label;
 import org.togglz.core.context.FeatureContext;
 
 public enum Features implements Feature {
 
     @Label("just a description")
-    //@FeatureGroup("zwei)
+    @FeatureGroup("zwei")
     @EnabledByDefault
     HELLO_WORLD,
 
     @Label("another description")
-    //@FeatureGroup("eins")
+    @FeatureGroup("eins")
     REVERSE_GREETING;
 
     public boolean isActive() {
