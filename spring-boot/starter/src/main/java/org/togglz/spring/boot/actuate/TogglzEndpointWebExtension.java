66891b1622bbea36b1e7a05b541abca6b1728e94
Peter Triller <ptriller@soapwars.de>
2022-10-23 15:43:38 +0200

    Add WebEndpoint extension for better error handling and integration tests for HTTP and JMX endpoint testing

---
+++ spring-boot/starter/src/main/java/org/togglz/spring/boot/actuate/TogglzEndpointWebExtension.java
@@ -0,0 +1,50 @@
+package org.togglz.spring.boot.actuate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.actuate.endpoint.annotation.Selector;
+import org.springframework.boot.actuate.endpoint.annotation.WriteOperation;
+import org.springframework.boot.actuate.endpoint.web.WebEndpointResponse;
+import org.springframework.boot.actuate.endpoint.web.annotation.EndpointWebExtension;
+import org.springframework.lang.Nullable;
+import org.togglz.core.Feature;
+import org.togglz.core.manager.FeatureManager;
+import org.togglz.core.metadata.FeatureMetaData;
+import org.togglz.core.repository.FeatureState;
+import org.togglz.spring.boot.actuate.autoconfigure.TogglzFeature;
+import org.togglz.spring.boot.actuate.autoconfigure.TogglzFeatureMetaData;
+
+import java.util.Map;
+
+/**
+ * Extension to the Togglz endpoint for HTTP so the status coddes for write operations are correct.
+ */
+@EndpointWebExtension(endpoint = TogglzEndpoint.class)
+public class TogglzEndpointWebExtension extends AbstractTogglzEndpoint {
+
+    private final Logger log = LoggerFactory.getLogger(TogglzEndpointWebExtension.class);
+
+
+    public TogglzEndpointWebExtension(FeatureManager featureManager) {
+        super(featureManager);
+    }
+
+
+    @WriteOperation
+    public WebEndpointResponse<TogglzFeature> setFeatureState(@Selector String name, @Nullable Boolean enabled,
+                                                              @Nullable String strategy, @Nullable String parameters) {
+        Feature feature = findFeature(name);
+        if (feature == null) {
+            return new WebEndpointResponse<>(WebEndpointResponse.STATUS_NOT_FOUND);
+        }
+        try {
+            Map<String, String> parametersMap = parseParameterMap(parameters);
+            FeatureState featureState = changeFeatureStatus(feature, enabled, strategy, parametersMap);
+            FeatureMetaData metaData = this.featureManager.getMetaData(feature);
+            return new WebEndpointResponse<>(new TogglzFeature(feature, featureState, new TogglzFeatureMetaData(metaData)));
+        } catch (IllegalArgumentException exception) {
+            log.debug("Illegal params format in togglz endpoint.", exception);
+            return new WebEndpointResponse<>(WebEndpointResponse.STATUS_BAD_REQUEST);
+        }
+    }
+}
--- spring-boot/starter/src/test/java/org/togglz/spring/boot/actuate/TogglzSpringBootEndpointTest.java
+++ spring-boot/starter/src/test/java/org/togglz/spring/boot/actuate/TogglzSpringBootEndpointTest.java
@@ -8,16 +8,23 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jmx.support.JmxUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.util.StreamUtils;
 import org.togglz.core.Feature;
 import org.togglz.core.annotation.*;
 
+import javax.management.MBeanException;
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UncheckedIOException;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -26,6 +33,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(
         properties = {
                 "management.endpoints.web.exposure.include=togglz",
+                "management.endpoints.jmx.exposure.include=togglz",
+                "spring.main.banner-mode=off",
+                "spring.jmx.enabled=true",
                 "togglz.feature-enums=org.togglz.spring.boot.actuate.TogglzSpringBootEndpointTest.TestFeatures"
         })
 @AutoConfigureMockMvc
@@ -40,7 +50,7 @@ public class TogglzSpringBootEndpointTest {
         FEATURE_ONE,
 
         @EnabledByDefault
-        FEATURE_TWO;
+        FEATURE_TWO
     }
 
     @Configuration
@@ -51,25 +61,94 @@ public class TogglzSpringBootEndpointTest {
     @Autowired
     private MockMvc mockMvc;
 
+
     @Test
-    void testActuatorEndpoint() throws Exception {
-        mockMvc
-                .perform(get("/actuator/togglz"))
+    void testWebActuatorEndpoint() throws Exception {
+        mockMvc.perform(get("/actuator/togglz"))
                 .andExpect(status().isOk())
                 .andExpect(content().json(readJson("all-features-initial.json")));
-        mockMvc
-                .perform(get("/actuator/togglz/FEATURE_ONE"))
+
+        mockMvc.perform(get("/actuator/togglz/FEATURE_ONE"))
                 .andExpect(status().isOk())
                 .andExpect(content().json(readJson("feature-one-initial.json")));
-        mockMvc
-                .perform(post("/actuator/togglz/FEATURE_ONE")
+
+        mockMvc.perform(post("/actuator/togglz/FEATURE_ONE")
                         .contentType("application/json")
                         .content("{ \"enabled\": true }"))
                 .andExpect(status().isOk());
-        mockMvc
-                .perform(get("/actuator/togglz/FEATURE_ONE"))
+
+        mockMvc.perform(get("/actuator/togglz/FEATURE_ONE"))
                 .andExpect(status().isOk())
-                .andExpect(content().json(readJson("feature-one-after.json")));
+                .andExpect(content().json(readJson("feature-one-enabled.json")));
+
+        mockMvc.perform(post("/actuator/togglz/FEATURE_ONE")
+                        .contentType("application/json")
+                        .content("{ \"strategy\": \"aStrategy\" }"))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/actuator/togglz/FEATURE_ONE"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(readJson("feature-one-new-strategy.json")));
+
+        mockMvc.perform(post("/actuator/togglz/FEATURE_ONE")
+                        .contentType("application/json")
+                        .content("{ \"parameters\": \"param1 = 10, param2 = 20 30, param3 = 40\" }"))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/actuator/togglz/FEATURE_ONE"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(readJson("feature-one-strategy-params.json")));
+
+        mockMvc.perform(get("/actuator/togglz/FEATURE_ABSENT"))
+                .andExpect(status().isNotFound());
+
+        mockMvc.perform(post("/actuator/togglz/FEATURE_ABSENT")
+                        .contentType("application/json")
+                        .content("{ \"parameters\": \"param1 = 10, param2 = 20 30, param3 = 40\" }"))
+                .andExpect(status().isNotFound());
+
+        mockMvc.perform(post("/actuator/togglz/FEATURE_ONE")
+                        .contentType("application/json")
+                        .content("{ \"parameters\": \"param1: 10\" }"))
+                .andExpect(status().isBadRequest());
+    }
+
+
+    @Test
+    @SuppressWarnings("unchecked")
+    void testJMXActuatorEndpoint() throws Exception {
+        MBeanServer beanServer = JmxUtils.locateMBeanServer();
+        ObjectName name = new ObjectName("org.springframework.boot:type=Endpoint,name=Togglz");
+
+        List<Map<String, Object>> allFeatures = (List<Map<String, Object>>) beanServer.invoke(name, "getAllFeatures", new Object[0], new String[0]);
+        assertEquals(2, allFeatures.size());
+
+        Map<String, Object> feature = (Map<String, Object>) beanServer.invoke(name, "getFeature", new Object[]{"FEATURE_ABSENT"}, new String[]{String.class.getName()});
+        assertNull(feature);
+
+        feature = (Map<String, Object>) beanServer.invoke(name, "getFeature", new Object[]{"FEATURE_TWO"}, new String[]{String.class.getName()});
+        assertTrue((Boolean) feature.get("enabled"));
+
+        feature = (Map<String, Object>) beanServer.invoke(name, "setFeatureState"
+                , new Object[]{"FEATURE_TWO", Boolean.FALSE, null, null},
+                new String[]{String.class.getName(), Boolean.class.getName(), String.class.getName(), String.class.getName()});
+        assertFalse((Boolean) feature.get("enabled"));
+
+        assertThrows(MBeanException.class, () -> beanServer.invoke(name, "setFeatureState"
+                , new Object[]{"FEATURE_ABSENT", Boolean.FALSE, null, null},
+                new String[]{String.class.getName(), Boolean.class.getName(), String.class.getName(), String.class.getName()}));
+
+        feature = (Map<String, Object>) beanServer.invoke(name, "setFeatureState"
+                , new Object[]{"FEATURE_TWO", null, null, "param1 = 10, param2 = 20 30, param3 = 40"},
+                new String[]{String.class.getName(), Boolean.class.getName(), String.class.getName(), String.class.getName()});
+        Map<String, String> params = (Map<String, String>) feature.get("params");
+        assertEquals("10", params.get("param1"));
+        assertEquals("20 30", params.get("param2"));
+        assertEquals("40", params.get("param3"));
+
+        assertThrows(MBeanException.class, () -> beanServer.invoke(name, "setFeatureState"
+                , new Object[]{"FEATURE_TWO", null, null, "param1: 10, param2 = 20 30, param3 = 40"},
+                new String[]{String.class.getName(), Boolean.class.getName(), String.class.getName(), String.class.getName()}));
     }
 
     private String readJson(String file) {
