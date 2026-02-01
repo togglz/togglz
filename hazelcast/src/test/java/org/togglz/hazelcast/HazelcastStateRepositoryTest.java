09c9dacb0f8959687dbabfd7759726eaaaf271f9
Erik Woelfel <erik.woelfel@otto.de>
2026-02-01 20:39:17 +0100

    Revert hazelcast update to 4.x again as the upgrade causes errors in the pipeline
    This reverts commit 5b0a850c

--- hazelcast/src/test/java/org/togglz/hazelcast/HazelcastStateRepositoryTest.java
+++ hazelcast/src/test/java/org/togglz/hazelcast/HazelcastStateRepositoryTest.java
@@ -1,22 +1,20 @@
 package org.togglz.hazelcast;
 
-import com.hazelcast.client.config.ClientConfig;
-import com.hazelcast.config.Config;
-import com.hazelcast.core.Hazelcast;
-import com.hazelcast.core.HazelcastInstance;
-import java.time.Duration;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
 import org.togglz.core.Feature;
 import org.togglz.core.repository.FeatureState;
 import org.togglz.core.repository.StateRepository;
 import org.togglz.core.util.NamedFeature;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
 
 class HazelcastStateRepositoryTest {
 
@@ -52,34 +50,29 @@ class HazelcastStateRepositoryTest {
     }
 
     @Test
-        // may fail if you are connected to a VPN
+    // may fail if you are connected to a VPN
     void test2nodeSetup() {
-        final Config config = new Config();
-        final HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance(config);
-        final StateRepository stateRepository1 = HazelcastStateRepository.newBuilder().mapName("togglzMap").hazelcastInstance(hazelcastInstance).build();
-        final StateRepository stateRepository2 = HazelcastStateRepository.newBuilder().mapName("togglzMap").hazelcastInstance(hazelcastInstance).build();
+        final StateRepository stateRepository1 = HazelcastStateRepository.newBuilder().mapName("togglzMap").build();
+        final StateRepository stateRepository2 = HazelcastStateRepository.newBuilder().mapName("togglzMap").build();
         final Feature feature = new NamedFeature("SAMPLE_FEATURE");
         final FeatureState featureState = new FeatureState(feature, false);
         stateRepository1.setFeatureState(featureState);
 
         assertFalse(stateRepository1.getFeatureState(feature).isEnabled());
-        //Wait until the second node has received the update
-        Awaitility.waitAtMost(Duration.ofSeconds(1)).until(() -> stateRepository2.getFeatureState(feature) != null);
         assertFalse(stateRepository2.getFeatureState(feature).isEnabled());
 
         featureState.setEnabled(true);
         stateRepository1.setFeatureState(featureState);
 
         assertTrue(stateRepository1.getFeatureState(feature).isEnabled());
-        //Wait until the second node has received the update
-        Awaitility.waitAtMost(Duration.ofSeconds(1)).until(() -> stateRepository2.getFeatureState(feature).isEnabled());
+        assertTrue(stateRepository2.getFeatureState(feature).isEnabled());
     }
 
     @Test
     void onlyConfigSetBuildsSuccessfully() {
         HazelcastStateRepository stateRepository = HazelcastStateRepository.newBuilder()
-                .config(new Config())
-                .build();
+          .config(new Config())
+          .build();
 
         assertNotNull(stateRepository);
     }
@@ -87,8 +80,8 @@ class HazelcastStateRepositoryTest {
     @Test
     void onlyClientConfigSetBuildsSuccessfully() {
         HazelcastStateRepository stateRepository = HazelcastStateRepository.newBuilder()
-                .clientConfig(new ClientConfig())
-                .build();
+          .clientConfig(new ClientConfig())
+          .build();
 
         assertNotNull(stateRepository);
     }
@@ -96,8 +89,8 @@ class HazelcastStateRepositoryTest {
     @Test
     void onlyHazelcastInstanceSetBuildsSuccessfully() {
         HazelcastStateRepository stateRepository = HazelcastStateRepository.newBuilder()
-                .hazelcastInstance(Hazelcast.newHazelcastInstance())
-                .build();
+          .hazelcastInstance(Hazelcast.newHazelcastInstance())
+          .build();
 
         assertNotNull(stateRepository);
     }
@@ -105,8 +98,8 @@ class HazelcastStateRepositoryTest {
     @Test
     void multipleConfiguredPiecesThrowsIllegalStateException() {
         assertThrows(IllegalStateException.class, () -> HazelcastStateRepository.newBuilder()
-                .clientConfig(new ClientConfig())
-                .config(new Config())
-                .build());
+          .clientConfig(new ClientConfig())
+          .config(new Config())
+          .build());
     }
 }
